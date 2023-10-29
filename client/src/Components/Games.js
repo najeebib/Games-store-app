ad52f0d4ddb55c329612d46253b04300d23093d0
najeebib <najeebibraheem1999@hotmail.com>
2023-10-29 14:30:21 +0200

    added games list page design

--- client/src/Components/Games.js
+++ client/src/Components/Games.js
@@ -5,6 +5,237 @@ function Games({category}) {
   return (
     <div>
       <Navbar/>
+      <div class="section">
+        <div class="container">
+        <div class="row">
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="..\server\Images\Games\AC_Mirage.jpg" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-xs-6">
+					<div class="product">
+						<div class="product-img">
+							<img src="./img/product02.png" alt=""/>
+						</div>
+						<div class="product-body">
+							<h3 class="product-name"><a href="#">product name goes here</a></h3>
+							<h4 class="product-price">$980.00</h4>
+							
+						</div>
+						<div class="add-to-cart">
+							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+						</div>
+					</div>
+				</div>
+				
+				
+			</div>
+        </div>
+      </div>
     </div>
   )
 }
