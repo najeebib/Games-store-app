6b590cde8fb227b0c86ea750ba608349418644c8
najeebib <najeebibraheem1999@hotmail.com>
2023-10-30 11:30:16 +0200

    display images from server side on the client side

--- client/src/Components/Games.js
+++ client/src/Components/Games.js
@@ -5,14 +5,14 @@ import Game from './Game';
 import './Games.css'
 function Games({category}) {
 	const products = [
-		{ gameName: 'Game 1', imageSource: 'image1.jpg', price: 49.99 },
-		{ gameName: 'Game 2', imageSource: 'image2.jpg', price: 39.99 },
-		{ gameName: 'Game 3', imageSource: 'image3.jpg', price: 59.99 },
-		{ gameName: 'Game 4', imageSource: 'image4.jpg', price: 29.99 },
-		{ gameName: 'Game 5', imageSource: 'image5.jpg', price: 69.99 },
-		{ gameName: 'Game 6', imageSource: 'image6.jpg', price: 79.99 },
-		{ gameName: 'Game 7', imageSource: 'image7.jpg', price: 89.99 },
-		{ gameName: 'Game 8', imageSource: 'image8.jpg', price: 99.99 },
+		{ gameName: 'Game 1', imageSource: 'http://localhost:5000/Images/AC_Mirage.jpg', price: 49.99 },
+		{ gameName: 'Game 2', imageSource: 'http://localhost:5000/Images/AC_Mirage.jpg', price: 39.99 },
+		{ gameName: 'Game 3', imageSource: 'http://localhost:5000/Images/AC_Mirage.jpg', price: 59.99 },
+		{ gameName: 'Game 4', imageSource: 'http://localhost:5000/Images/AC_Mirage.jpg', price: 29.99 },
+		{ gameName: 'Game 5', imageSource: 'http://localhost:5000/Images/AC_Mirage.jpg', price: 69.99 },
+		{ gameName: 'Game 6', imageSource: 'http://localhost:5000/Images/AC_Mirage.jpg', price: 79.99 },
+		{ gameName: 'Game 7', imageSource: 'http://localhost:5000/Images/AC_Mirage.jpg', price: 89.99 },
+		{ gameName: 'Game 8', imageSource: 'http://localhost:5000/Images/AC_Mirage.jpg', price: 99.99 },
 	  ];
   return (
     <div>
--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
 import router from "./Routes/Routes.js";
+import path from 'path'
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname).slice(1);
+console.log(path.join(__dirname, ''))
 const app = express();
 app.use(cors());
 app.use(express.json());  // Apply express.json() middleware here
 app.use(express.urlencoded({ extended: true }));
-app.use('/', router);
+app.use('/Images', express.static(path.join(__dirname, 'Images')));
 
 // ... other app configurations
+app.use('/', router);
 
 app.listen(5000, () => {
   console.log("Listening on port 5000");
